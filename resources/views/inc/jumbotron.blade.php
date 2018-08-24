f53982b23bb66f1c0104bf60b3214b4dd2c93c7a
JT Maingi <jtmaingi@gmail.com>
2018-08-24 11:04:30 +0300

    jumbotron edits and custom css

--- resources/views/inc/jumbotron.blade.php
+++ resources/views/inc/jumbotron.blade.php
@@ -1,6 +1,6 @@
 <div class="jumbotron jumbotron-fluid text-white text-center">
-    <div class="container">
-          <h1 class="display-2"> I Got TALENT </h1>
+    <div class="container py-5">
+          <h1 class="display-3"> I Got TALENT </h1>
           <p class="lead"> We Identify, Appreciate and Nurture Talents of the young generation across the 47 Counties of Kenya. </p> 
          </div>
       </div>
